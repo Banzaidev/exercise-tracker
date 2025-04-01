84ea76298d7110e86209c8993bd72c573e22e0d3
Banzaidev <66788936+Banzaidev@users.noreply.github.com>
2025-04-01 21:22:54 +0200

    feat: add mongoose connection and users schema with unique username

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 require('dotenv').config()
+
+mongoose.connect(process.env.DB_URI)
+
+const usersSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+})
 
 app.use(cors())
 app.use(express.static('public'))
